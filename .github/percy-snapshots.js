17b729fa9f7c5f378c2be436c5bff87235f2b34a
Buck Doyle <b@chromatin.ca>
2020-11-11 13:17:05 -0600

    Add preliminary Percy snapshots script

---
+++ .github/percy-snapshots.js
@@ -0,0 +1,17 @@
+const PercyScript = require('@percy/script');
+
+const HOST = process.argv[2] || 'https://ghost-theme-sandbox-base.herokuapp.com/';
+
+PercyScript.run(async (page, percySnapshot) => {
+    await page.goto(`${HOST}`);
+    await page.waitFor('footer');
+    await percySnapshot('homepage');
+
+    await page.goto(`${HOST}blog/a-post/`);
+    await page.waitFor('h3');
+    await percySnapshot('blog-post');
+
+    await page.goto(`${HOST}a-page/`);
+    await page.waitFor('h3');
+    await percySnapshot('page');
+});
