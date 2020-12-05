11ea1fb5c860dae623dbfbb239df2343547d3dfd
Farfama <32600047+FarfamaH@users.noreply.github.com>
2020-12-05 17:45:03 -0600

    Merge main into production (#48)
    * Update version of branch-name action (#42)
    
    This includes a fix for a deprecation warning about ::set-env.
    
    * Add visual diffs with Percy (#46)
    
    This adds a job to the preview workflow to generate Percy visual diffs from the preview
    deployment so it’s easier to see what effects CSS changes have.
    
    * Fix aspect ratio of post image (#47)
    
    Co-authored-by: Buck Doyle <b@chromatin.ca>

---
+++ .github/percy-snapshots.js
@@ -0,0 +1,18 @@
+const PercyScript = require('@percy/script');
+
+const HOST = process.env.HEROKU_APP_HOST || 'https://ghost-theme-sandbox-base.herokuapp.com/';
+console.log(`Collecting Percy snapshots from host ${HOST}…`);
+
+PercyScript.run(async (page, percySnapshot) => {
+    await page.goto(`${HOST}`);
+    await page.waitFor('footer');
+    await percySnapshot('homepage');
+
+    await page.goto(`${HOST}blog/a-post/`);
+    await page.waitFor('h3');
+    await percySnapshot('blog-post');
+
+    await page.goto(`${HOST}a-page/`);
+    await page.waitFor('h3');
+    await percySnapshot('page');
+});
